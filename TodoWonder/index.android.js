a36facd989ecc7ed3b94b213e930e4a1e5ada80a
Linton Ye <linton@jimulabs.com>
2016-06-09 15:40:44 -0700

    Update to use rn 0.28.0-rc.0

--- TodoWonder/index.android.js
+++ TodoWonder/index.android.js
@@ -6,5 +6,7 @@ import {
   AppRegistry,
 } from 'react-native';
 import TodoWonderApp from './js/TodoWonderApp'
+// import YourApplication from './js/NavigationHeaderScenesTabs-example'
 
 AppRegistry.registerComponent('TodoWonder', () => TodoWonderApp);
+// AppRegistry.registerComponent('TodoWonder', () => YourApplication);
